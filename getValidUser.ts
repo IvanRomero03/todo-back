4935001c922234f947203dedd1f25bd4988b4017
IvanRomero03 <i.wells.ar@gmail.com>
2022-07-20 14:16:58 -0700

    user fix

--- getValidUser.ts
+++ getValidUser.ts
@@ -6,13 +6,13 @@ const getValidUser = (con, req, res) => {
     res.sendStatus(400);
   } else {
     con.query(
-      `SELECT COUNT(*) FROM userTable WHERE name = '${name}' AND password = '${password}';`,
+      `SELECT userId FROM usersTable WHERE name = '${name}' AND password = '${password}';`,
       (err, result) => {
         if (err) {
           console.log(err);
           res.sendStatus(500);
         } else {
-          if (result[0]["COUNT(*)"] === 1) {
+          if (result[0]["userId"] === 1) {
             res.status(200).json(true);
           } else {
             res.status(200).json(false);
