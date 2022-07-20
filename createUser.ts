db3a11e37c67d180d4657053ae9e7fde54b3d39f
IvanRomero03 <i.wells.ar@gmail.com>
2022-07-20 14:11:24 -0700

    create user update

--- createUser.ts
+++ createUser.ts
@@ -6,12 +6,13 @@ const createUser = (con, req, res) => {
     res.sendStatus(400);
   } else {
     con.query(
-      `INSERT INTO userTable (name, password) VALUES ('${name}', '${password}');`,
+      `INSERT INTO usersTable (name, password) VALUES ('${name}', '${password}');`,
       (err, result) => {
         if (err) {
           console.log(err);
           res.sendStatus(500);
         } else {
+          console.log(result);
           res.status(200).json(result);
         }
       }
