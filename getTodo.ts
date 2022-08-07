b0feaf0dd2a9ea7a838f44d862845a98f29cebb6
IvanRomero03 <i.wells.ar@gmail.com>
2022-08-06 19:12:57 -0500

    column correction

--- getTodo.ts
+++ getTodo.ts
@@ -1,12 +1,15 @@
 const getTodo = (con, req, res) => {
   const idTodo = req.body.idTodo;
-  con.query(`SELECT * FROM todoTable WHERE id = ${idTodo};`, (err, result) => {
-    if (err) {
-      console.log(err);
-      res.sendStatus(500);
-    } else {
-      res.status(200).json(result);
+  con.query(
+    `SELECT * FROM todoTable WHERE idTodo = ${idTodo};`,
+    (err, result) => {
+      if (err) {
+        console.log(err);
+        res.sendStatus(500);
+      } else {
+        res.status(200).json(result);
+      }
     }
-  });
+  );
 };
 module.exports = getTodo;
